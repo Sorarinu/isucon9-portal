ccd2243b094f4ce21b6f78d1a9363fc78381aaa8
Youta EGUSA <y-egusa@sakura.ad.jp>
2019-05-31 17:07:45 +0900

    logout

--- isucon/portal/authentication/urls.py
+++ isucon/portal/authentication/urls.py
@@ -5,4 +5,5 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name="login.html")),
+    path('logout/', auth_views.LogoutView.as_view(template_name="logout.html")),
 ]
