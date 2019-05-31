b3dfe0d84f57b0e5fbb3060a0977fc0f11efbf71
Youta EGUSA <y-egusa@sakura.ad.jp>
2019-05-31 17:05:15 +0900

    index

--- isucon/portal/urls.py
+++ isucon/portal/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from isucon.portal import views
+
 urlpatterns = [
+    path('', views.index),
     path('auth/', include("isucon.portal.authentication.urls")),
     path('admin/', admin.site.urls),
 ]
