a6065ea807caaaea459f5f25cd044c134acc1846
Youta EGUSA <y-egusa@sakura.ad.jp>
2019-05-31 18:11:13 +0900

    ダミーコンテキストの作成

--- isucon/portal/contest/views.py
+++ isucon/portal/contest/views.py
@@ -5,18 +5,63 @@ from isucon.portal.authentication.decorators import team_is_authenticated
 def index(request):
     return render(request, "index.html")
 
+
+def get_base_context(user):
+    # FIXME: Dummy
+    target_server = None
+    is_last_spurt = False
+
+    return {
+        "target_server": target_server,
+        "is_last_spurt": is_last_spurt,
+    }
+
 @team_is_authenticated
 def dashboard(request):
-    return render(request, "dashboard.html")
+    context = get_base_context(request.user)
+
+    # FIXME: Query
+    recent_jobs = []
+    top_teams = []
+
+    context.update({
+        "recent_jobs": recent_jobs,
+        "top_teams": top_teams,
+    })
+    return render(request, "dashboard.html", context)
 
 @team_is_authenticated
 def jobs(request):
-    return render(request, "jobs.html")
+    context = get_base_context(request.user)
+
+    # FIXME: Query
+    jobs = []
+
+    context.update({
+        "jobs": jobs,
+    })
+    return render(request, "jobs.html", context)
 
 @team_is_authenticated
 def scores(request):
-    return render(request, "scores.html")
+    context = get_base_context(request.user)
+
+    # FIXME: Query
+    teams = []
+
+    context.update({
+        "teams": teams,
+    })
+    return render(request, "scores.html", context)
 
 @team_is_authenticated
 def servers(request):
-    return render(request, "servers.html")
+    context = get_base_context(request.user)
+
+    # FIXME: Query
+    servers = []
+
+    context.update({
+        "servers": servers,
+    })
+    return render(request, "servers.html", context)
