578f459ccb348721049c083e61055750a4adb71a
k-egawa <k-egawa@sakura.ad.jp>
2019-05-31 15:13:42 +0900

    外部キー修正

--- isucon/portal/resources/admin.py
+++ isucon/portal/resources/admin.py
@@ -3,14 +3,14 @@ from django.contrib import admin
 from isucon.portal.resources.models import Server, Benchmarker
 
 class ServerAdmin(admin.ModelAdmin):
-    list_display = ["id", "hostname", "global_ip", "private_ip", "private_network"]
+    list_display = ["id", "hostname", "global_ip", "private_ip", "private_network", "benchmarker"]
     list_filter = ["hostname"]
 
 admin.site.register(Server, ServerAdmin)
 
 
 class BenchmarkerAdmin(admin.ModelAdmin):
-    list_display = ["id", "network", "server", "node"]
+    list_display = ["id", "network", "node"]
     list_filter = ["node"]
 
 admin.site.register(Benchmarker, BenchmarkerAdmin)
--- isucon/portal/resources/models.py
+++ isucon/portal/resources/models.py
@@ -2,6 +2,22 @@ from django.db import models
 
 from isucon.portal.models import LogicalDeleteMixin
 
+
+class Benchmarker(LogicalDeleteMixin, models.Model):
+    class Meta:
+        verbose_name = verbose_name_plural = "ベンチマーカー"
+
+    # FIXME: ネットワーク構成により今後変更
+    ip = models.CharField("IPアドレス", max_length=100)
+    network = models.CharField("ネットワークアドレス", max_length=100)
+
+    # FIXME: これってなんのフィールドなのかまだわかってない
+    node = models.CharField("ノード", max_length=100)
+
+    def __str__(self):
+        return self.ip
+
+
 class Server(LogicalDeleteMixin, models.Model):
     class Meta:
         verbose_name = verbose_name_plural = "サーバ"
@@ -16,22 +32,7 @@ class Server(LogicalDeleteMixin, models.Model):
     private_ip = models.CharField("プライベートIPアドレス", max_length=100)
     private_network = models.CharField("プライベートネットワークアドレス", max_length=100)
 
+    benchmarker = models.ForeignKey(Benchmarker, verbose_name="ベンチマーカー", on_delete=models.PROTECT)
+
     def __str__(self):
         return self.hostname
-
-
-class Benchmarker(LogicalDeleteMixin, models.Model):
-    class Meta:
-        verbose_name = verbose_name_plural = "ベンチマーカー"
-
-    # FIXME: ネットワーク構成により今後変更
-    ip = models.CharField("IPアドレス", max_length=100)
-    network = models.CharField("ネットワークアドレス", max_length=100)
-
-    server = models.ForeignKey(Server, verbose_name="ベンチマーク対象サーバ", on_delete=models.PROTECT)
-
-    # FIXME: これってなんのフィールドなのかまだわかってない
-    node = models.CharField("ノード", max_length=100)
-
-    def __str__(self):
-        return self.ip
