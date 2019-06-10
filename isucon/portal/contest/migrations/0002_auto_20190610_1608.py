85e28f289f427a98296a0c5f43dec84145a2533f
k-egawa <k-egawa@sakura.ad.jp>
2019-06-10 16:17:12 +0900

    マイグレーション修正

--- isucon/portal/contest/migrations/0002_auto_20190610_1608.py
+++ isucon/portal/contest/migrations/0002_auto_20190610_1608.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('authentication', '0006_auto_20190610_1608'),
+        ('authentication', '0006_auto_20190603_1824'),
         ('contest', '0001_initial'),
     ]
 
