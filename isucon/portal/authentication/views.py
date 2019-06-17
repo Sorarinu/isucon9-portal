e8bc9a565c40b1c35eaa0db231df0a43ef876616
misodengaku <misodengaku@gmail.com>
2019-06-17 18:00:46 +0900

    すごく雑にチーム作成と参加できるようにした

---
+++ isucon/portal/authentication/views.py
@@ -0,0 +1,56 @@
+import random
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+
+from isucon.portal.authentication.models import Team
+
+PASSWORD_LETTERS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*'
+PASSWORD_LENGTH = 20
+
+@login_required
+def create_team(request):
+    user = request.user
+    team_name = request.POST['team_name']
+    
+    if len(Team.objects.filter(name=request.POST['team_name'])) > 0:
+        raise RuntimeError('team name already exists')
+    
+    password = ''.join(random.choice(PASSWORD_LETTERS) for i in range(PASSWORD_LENGTH)) 
+
+    team = Team.objects.create(name=request.POST['team_name'], password=password, owner=user)
+    
+    user.team = Team.objects.get(name=request.POST['team_name'])
+    user.save()
+
+
+    context = {
+        "team_id": team.id,
+        "team_name": team_name,
+        "team_password": password,
+    }
+    
+    return render(request, "team_created.html", context)
+
+@login_required
+def join_team(request):
+    user = request.user
+    team_id = request.POST['team_id']
+    team_password = request.POST['team_password']
+    
+    team = Team.objects.get(id=int(team_id), password=team_password)
+
+    if team is None:
+        raise RuntimeError('team information is invalid')
+    
+    user.team = team
+    user.save()
+
+
+    context = {
+        "team_name": team.name,
+        "team_id": team.id,
+    }
+    
+    return render(request, "team_joined.html", context)
