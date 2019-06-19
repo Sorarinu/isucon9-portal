492420e38d697c3c67f049afb6ce391b90665237
k-egawa <k-egawa@sakura.ad.jp>
2019-06-19 12:50:51 +0900

    AggregatedScoreの追加(WIP)

--- isucon/portal/contest/signals.py
+++ isucon/portal/contest/signals.py
@@ -8,10 +8,11 @@ from isucon.portal.contest.models import AggregatedScore
 @receiver(post_save, sender=Team)
 def create_default_aggregated_score(sender, instance, created, **kwargs):
     """チームに対し、デフォルトの `AggregatedScore` を作成する"""
-    # 集計スコア作成
-    aggregated_score = AggregatedScore.objects.create()
-    aggregated_score.save()
+    if created:
+        # 集計スコア作成
+        aggregated_score = AggregatedScore.objects.create()
+        aggregated_score.save()
 
-    # 集計スコアとチームを紐付け
-    instance.aggregated_score = aggregated_score
-    instance.save()
+        # 集計スコアとチームを紐付け
+        instance.aggregated_score = aggregated_score
+        instance.save()
