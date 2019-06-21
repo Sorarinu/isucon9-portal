d2ee6a88563a02a8e8ea888434af5e219992e6e2
k-egawa <k-egawa@sakura.ad.jp>
2019-06-21 16:23:18 +0900

    シードデータ生成の実装

---
+++ isucon/portal/authentication/factories.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.hashers import make_password
+
+import factory
+import factory.fuzzy
+
+from isucon.portal.authentication import models
+from isucon.portal.contest import factories as contest_factories
+
+
+class UserFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = models.User
+
+    username = factory.Sequence(lambda idx: "user{}".format(idx))
+
+class TeamFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = models.Team
+
+    # NOTE: owner以外は、別スクリプトで設定
+    owner = factory.SubFactory(UserFactory)
+    benchmarker = factory.SubFactory(contest_factories.BenchmarkerFactory)
+
+    name = factory.Sequence(lambda idx: "team{}".format(idx))
+    password = factory.Sequence(lambda idx: make_password("password{}".format(idx)))
+
+    is_active = True
---
+++ isucon/portal/contest/factories.py
@@ -0,0 +1,82 @@
+import datetime
+import random
+
+import factory
+import factory.fuzzy
+from faker import Faker
+
+from isucon.portal.contest import models
+
+
+def random_ip(idx):
+    ipv4 = Faker().ipv4()
+    octets = ipv4.split('.')[:-1]
+    octets.append(idx)
+    return '.'.join(map(str, octets))
+
+
+class BenchmarkerFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = models.Benchmarker
+
+    ip = factory.Sequence(lambda idx: random_ip(idx))
+
+
+class ServerFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = models.Server
+
+    # NOTE: 紐づくチームは contest.management.commands.manufacture にて設定
+    hostname = factory.Sequence(lambda idx: "server{}".format(idx))
+
+    global_ip = factory.Sequence(lambda idx: random_ip(idx))
+    private_ip = factory.Sequence(lambda idx: random_ip(idx))
+
+
+class JobFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = models.Job
+
+    log_text = factory.Faker('sentence')
+
+    created_at = factory.fuzzy.FuzzyDate(
+        start_date=datetime.datetime.now()-datetime.timedelta(days=5),
+        end_date=datetime.datetime.now()-datetime.timedelta(days=3),
+    )
+    updated_at = factory.fuzzy.FuzzyDate(
+        start_date=datetime.datetime.now()-datetime.timedelta(days=2),
+        end_date=datetime.datetime.now()-datetime.timedelta(days=1),
+    )
+
+    @factory.lazy_attribute
+    def result_json(self):
+        if self.status == models.Job.ABORTED:
+            return '{"reason": "Benchmark timeout"}'
+
+        if self.is_passed:
+            pass_str = "true"
+        else:
+            pass_str = "false"
+
+        return '{{ "score": {}, "pass": {} }}'.format(self.score, pass_str)
+
+
+class ScoreHistoryFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = models.ScoreHistory
+
+    # NOTE: 紐づくチームは contest.management.commands.manufacture にて設定
+    job = factory.LazyAttribute(lambda o: JobFactory(
+        team=o.team,
+        score=o.score,
+        is_passed=o.is_passed,
+        status=models.Job.DONE)
+    )
+    is_passed = factory.fuzzy.FuzzyChoice([True, False])
+
+    @factory.lazy_attribute
+    def score(self):
+        if self.is_passed:
+            return random.randint(10, 90000)
+
+        return 0
