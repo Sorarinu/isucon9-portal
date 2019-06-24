9732f4e73abb1832eade3cde9dc1c2ed71d59f59
k-egawa <k-egawa@sakura.ad.jp>
2019-06-24 19:43:38 +0900

    ロギング追加

--- isucon/portal/contest/management/commands/discard_forever.py
+++ isucon/portal/contest/management/commands/discard_forever.py
@@ -1,8 +1,12 @@
 import time
+import logging
 import pprint
 from django.core.management.base import BaseCommand
 
 from isucon.portal.contest.models import Job
+
+
+logger = logging.getLogger("discard_forever")
 
 
 class Command(BaseCommand):
@@ -10,19 +14,14 @@ class Command(BaseCommand):
     def discard_timeout_jobs(self):
         try:
             jobs = Job.objects.discard_timeout_jobs()
+            if len(jobs) > 0:
+                logger.info("discard jobs")
+                pprint.pprint(jobs)
         except Exception as e:
             # through exception
             print(e)
-            jobs = []
-
-        return jobs
 
     def handle(self, *args, **options):
         while True:
-            jobs = self.discard_timeout_jobs()
-            if len(jobs) > 0:
-                print("Cleanup jobs:")
-                pprint.pprint(jobs)
-                print("=====")
-
+            self.discard_timeout_jobs()
             time.sleep(1)
