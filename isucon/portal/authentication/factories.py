07c8ee15c220f9a3e19addbd39555c151d4935f6
k-egawa <k-egawa@sakura.ad.jp>
2019-06-25 12:08:20 +0900

    is_studentを散らばるように

--- isucon/portal/authentication/factories.py
+++ isucon/portal/authentication/factories.py
@@ -12,7 +12,9 @@ class UserFactory(factory.DjangoModelFactory):
         model = models.User
 
     username = factory.Sequence(lambda idx: "user{}".format(idx))
-    is_student = factory.fuzzy.FuzzyChoice([True, False])
+    # NOTE: 最大３人のチームで、３チームに１人くらい学生がいる
+    # なお、シード生成の時にチームの人数がランダムになるので、そこで調整が入り、学生の増減がでる
+    is_student = factory.Sequence(lambda idx: idx % 9 == 0)
 
 class TeamFactory(factory.DjangoModelFactory):
     class Meta:
