47366d0d58baa5edbbc1a80677eeb37a52072a8f
k-egawa <k-egawa@sakura.ad.jp>
2019-06-25 17:06:26 +0900

    無駄な差分を消した

--- isucon/portal/settings.py
+++ isucon/portal/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-from django.utils import timezone
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
