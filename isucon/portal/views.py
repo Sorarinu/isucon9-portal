b3362b97c5875649b13f6a7bffd62e26a561b1a5
k-egawa <k-egawa@sakura.ad.jp>
2019-06-26 15:51:54 +0900

    登録可能チェックをlanding.htmlに入れた

--- isucon/portal/views.py
+++ isucon/portal/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 
+from isucon.portal.authentication.decorators import is_registration_available
+
 def index(request):
-    return render(request, "landing.html")
+    return render(request, "landing.html", {'is_registration_available': is_registration_available()})
