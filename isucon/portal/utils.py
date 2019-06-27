8c7e5d316340d4884bf31001e06f697760d82aa7
k-egawa <k-egawa@sakura.ad.jp>
2019-06-27 14:38:31 +0900

    引数名変更

--- isucon/portal/utils.py
+++ isucon/portal/utils.py
@@ -7,7 +7,7 @@ def get_utc_time(hour, minute, second):
             .astimezone(datetime.timezone.utc)\
             .time()
 
-def get_utc_datetime(year, month, date, hour, minute, second):
+def get_utc_datetime(year, month, day, hour, minute, second):
     """指定日時をUTCとして取得"""
-    return datetime.datetime(year, month, date, hour, minute, second)\
+    return datetime.datetime(year, month, day, hour, minute, second)\
             .astimezone(datetime.timezone.utc)
