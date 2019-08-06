4bf5a1418571d238031415cfc1bd72342b7c80ad
Youta EGUSA <y-egusa@sakura.ad.jp>
2019-08-06 11:33:08 +0900

    チーム名の変更

--- isucon/portal/contest/forms.py
+++ isucon/portal/contest/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.core.validators import RegexValidator
 
+from isucon.portal.authentication.decorators import is_registration_available
 from isucon.portal.authentication.models import Team
 
 alibaba_account_validator = RegexValidator(r'^\d{16}$', "Invalid Account ID Format")
@@ -8,7 +9,20 @@ alibaba_account_validator = RegexValidator(r'^\d{16}$', "Invalid Account ID Form
 class TeamForm(forms.ModelForm):
     class Meta:
         model = Team
-        fields = ("alibaba_account", )
-
+        fields = ("name", "alibaba_account", )
 
     alibaba_account = forms.CharField(required=True, validators=[alibaba_account_validator], )
+
+    def __init__(self, *args, **kwargs):
+        self.is_registration_available = is_registration_available()
+
+        super().__init__(*args, **kwargs)
+
+        if not self.is_registration_available:
+            self.fields['name'].widget.attrs['readonly'] = True
+            self.fields['name'].widget.attrs['class'] = 'is-static'
+
+    def clean_name(self):
+        if not self.is_registration_available:
+            return self.instance.name
+        return self.cleaned_data.get("name", "")
