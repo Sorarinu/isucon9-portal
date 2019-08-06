e89dfda9096f3b553febcb0263759f918e42b21f
tukeJonny <ne250143@yahoo.co.jp>
2019-08-06 13:13:00 +0900

    TeamSerializerにまとめる

--- isucon/portal/internal/serializers.py
+++ isucon/portal/internal/serializers.py
@@ -1,25 +1,27 @@
 from rest_framework import serializers
 
-from isucon.portal.contest import models as contest_models
+from isucon.portal.authentication.models import Team
+from isucon.portal.contest.models import Server, Job
+
+
+class ServerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Server
+        fields = ('id', 'hostname', 'global_ip', 'private_ip', 'is_bench_target')
+
+
+class TeamSerializer(serializers.ModelSerializer):
+    """チームと、ベンチマークやblackboxで必要なサーバ情報のシリアライザ"""
+    servers = ServerSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Team
+        fields = ('id', 'owner', 'name', 'servers')
 
 
 class JobSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = contest_models.Job
-        fields = ('id', 'target_server_ip')
-
-    def get_target_server_ip(self, obj):
-        team = obj.team
-        target_server = contest_models.ServerManager.objects.get_bench_target(team)
-        return target_server.private_ip
-
-
-class JobResultSerializer(serializers.Serializer):
-    score = serializers.IntegerField(required=True)
-    is_passed = serializers.BooleanField(required=True)
-    stdout = serializers.CharField(required=True)
-    stderr = serializers.CharField(required=True)
-
-    def validate(self, data):
-        pass
+        model = Job
+        fields = ('id', 'score', 'is_passed', 'stdout', 'stderr')
--- isucon/portal/internal/views.py
+++ isucon/portal/internal/views.py
@@ -6,22 +6,12 @@ from rest_framework.decorators import detail_route
 from rest_framework.response import Response
 from ipware import get_client_ip
 
-from isucon.portal.contest import models as contest_models
-from isucon.portal.internal import serializers as internal_serializers
+from isucon.portal.contest.models import Benchmarker, Job
+from isucon.portal.internal.serializers import JobSerializer
 
 
 class JobViewSet(viewsets.GenericAPIViewSet):
-    serializer_class = internal_serializers.JobSerializer
-
-    def get_queryset(self):
-        client_ip, _ = get_client_ip(self.request)
-        if client_ip is None:
-            # FIXME: client_ip取得できなかったエラー
-            pass
-
-        queryset = contest_models.Job.objects.all()
-        benchmarker = get_object_or_404(queryset, ip=client_ip)
-        return contest_models.Job.objects.dequeue(benchmarker)
+    serializer_class = JobSerializer
 
     @detail_route(methods=['post'])
     def dequeue(self, request, *args, **kwargs):
@@ -31,13 +21,13 @@ class JobViewSet(viewsets.GenericAPIViewSet):
             return HttpResponse('IPアドレスが不正です', status.HTTP_400_BAD_REQUEST)
 
         try:
-            benchmarker = contest_models.Benchmarker.objects.get(ip=client_ip)
-        except contest_models.Benchmarker.DoesNotExist:
+            benchmarker = Benchmarker.objects.get(ip=client_ip)
+        except Benchmarker.DoesNotExist:
             return HttpResponse('登録されていないベンチマーカーです', status.HTTP_400_BAD_REQUEST)
 
-        job = contest_models.Job.dequeue(benchmarker)
+        job = Job.objects.dequeue(benchmarker)
         if job is None:
-            return HttpResponse('ジョブキューが空です', status.HTTP_404_NOT_FOUND)
+            return HttpResponse('ジョブキューが空です', status.HTTP_422_UNPROCESSABLE_ENTITY)
 
         job_serializer = self.serializer_class(instance=job)
         return Response(job_serializer.data)
