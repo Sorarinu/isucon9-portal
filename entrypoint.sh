da7ef06d901b6ac1b13a04c803b3b5ebb22a2e11
Youta EGUSA <y-egusa@sakura.ad.jp>
2019-09-03 11:00:21 +0900

    -w と --max-requests

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
+#!/bin/bash -x
+
+if [ -z "${NUM_WORKERS}" ]; then
+  NUM_WORKERS=2
+fi
+
+if [ -z "${MAX_REQUESTS}" ]; then
+  MAX_REQUESTS=1000
+fi
 
 python manage.py collectstatic --noinput
 python manage.py migrate
-gunicorn isucon.portal.wsgi:application -b 0.0.0.0:5000
+gunicorn isucon.portal.wsgi:application -b 0.0.0.0:5000 -w ${NUM_WORKERS} --max-requests ${MAX_REQUESTS}
