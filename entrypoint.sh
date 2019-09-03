6c9e8c435c4617e3e30bbbbf4f0d4e6fdc6c0534
Youta EGUSA <y-egusa@sakura.ad.jp>
2019-09-03 11:21:48 +0900

    add log to host

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,8 @@ fi
 
 python manage.py collectstatic --noinput
 python manage.py migrate
-gunicorn isucon.portal.wsgi:application -b 0.0.0.0:5000 -w ${NUM_WORKERS} --max-requests ${MAX_REQUESTS} --keep-alive 120 --access-logfile -
+gunicorn isucon.portal.wsgi:application \
+-b 0.0.0.0:5000 -w ${NUM_WORKERS} --max-requests ${MAX_REQUESTS} \
+--keep-alive 120 \
+--access-logfile /var/log/django/access.log \
+--error-logfile /var/log/django/error.log
