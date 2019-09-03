c57c670230b39c5878b451d00ea020c8a8d184de
tukeJonny <ne250143@yahoo.co.jp>
2019-09-03 12:15:48 +0900

    日付修正

--- isucon/portal/settings.py
+++ isucon/portal/settings.py
@@ -186,17 +186,17 @@ LOGGING = {
 
 # 登録期間
 REGISTRATION_START_AT = portal_utils.get_utc_datetime(2019, 7, 1, 9, 0, 0)
-REGISTRATION_END_AT = portal_utils.get_utc_datetime(2019, 10, 25, 9, 0, 0)
+REGISTRATION_END_AT = portal_utils.get_utc_datetime(2019, 8, 25, 9, 0, 0)
 
 # コンテスト開催期間
 # 日付
 CONTEST_DATES = [
-    datetime.date(2019, 9, 3),
-    datetime.date(2019, 9, 3)
+    datetime.date(2019, 9, 7),
+    datetime.date(2019, 9, 8)
 ]
 
 # 時刻
-CONTEST_START_TIME = portal_utils.get_utc_time(0, 0, 0)
+CONTEST_START_TIME = portal_utils.get_utc_time(9, 0, 0)
 CONTEST_END_TIME = portal_utils.get_utc_time(18, 0, 0)
 
 # Github認証に使うトークン
