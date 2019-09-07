e6050b9c3b43c269e784ddb9cbf5419b2519b924
tukeJonny <ne250143@yahoo.co.jp>
2019-09-07 23:15:39 +0900

    team_id KeyErrorのバグ修正

--- isucon/portal/settings.py
+++ isucon/portal/settings.py
@@ -188,7 +188,7 @@ LOGGING = {
 
 # 登録期間
 REGISTRATION_START_AT = portal_utils.get_jst_datetime(2019, 7, 1, 9, 0, 0)
-REGISTRATION_END_AT = portal_utils.get_jst_datetime(2019, 8, 25, 9, 0, 0)
+REGISTRATION_END_AT = portal_utils.get_jst_datetime(2019, 9, 25, 9, 0, 0)
 
 # コンテスト開催期間
 # 日付
