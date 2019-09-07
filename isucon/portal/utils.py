6c03e807aef2f4f20386230b378ea220091d0cea
chibiegg <chibiegg@chibiegg.net>
2019-09-07 09:45:18 +0900

    jst

--- isucon/portal/utils.py
+++ isucon/portal/utils.py
@@ -23,7 +23,8 @@ def is_last_spurt(t):
     t = t.astimezone(jst) # UTCに保証
     lookahead = t + timezone.timedelta(hours=1)
 
-    contest_end_time = settings.CONTEST_END_TIME + datetime.timedelta(hours=9)
+    contest_end_date = datetime.datetime.combine(datetime.date.today(), settings.CONTEST_END_TIME) + datetime.timedelta(hours=9)
+    contest_end_time = contest_end_date.time()
 
     if lookahead.time() >= contest_end_time:
         return True
