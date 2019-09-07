5c94c79bf932759e7f784f50d61209c554a4ff9a
chibiegg <chibiegg@chibiegg.net>
2019-09-07 10:39:34 +0900

    日付チェックミス

--- isucon/portal/authentication/models.py
+++ isucon/portal/authentication/models.py
@@ -54,12 +54,11 @@ class Team(LogicalDeleteMixin, models.Model):
             return True
 
         now = timezone.now().astimezone(jst)
-        start_time = datetime.datetime.combine(datetime.date.today(), settings.CONTEST_START_TIME)
-        end_time = datetime.datetime.combine(datetime.date.today(), settings.CONTEST_END_TIME)
+        start_time = datetime.datetime.combine(self.participate_at, settings.CONTEST_START_TIME)
+        end_time = datetime.datetime.combine(self.participate_at, settings.CONTEST_END_TIME)
 
         in_time = (start_time.time() <= now.time() <= end_time.time())
         in_date = (start_time.date() == now.date())
-        print(start_time.time(), now.time(), end_time.time())
         return in_time and in_date
 
     def __str__(self):
